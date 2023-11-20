e22c0be946de5d750c19c8763913160b02e5974a
Kotta64 <111344592+Kotta64@users.noreply.github.com>
2023-11-20 18:16:53 +0900

    RPC導入の際のミスを修正

--- Assets/Scripts/LeftRoomButton.cs
+++ Assets/Scripts/LeftRoomButton.cs
@@ -5,8 +5,6 @@ using UnityEngine.SceneManagement;
 public class LeftRoomButton : MonoBehaviour
 {
     public void OnClick() {
-        PhotonNetwork.CurrentRoom.setStartFlag(0);
-        PhotonNetwork.CurrentRoom.setStartWindow(false);
         PhotonNetwork.LeaveRoom();
         SceneManager.LoadScene("TitleScene");
     }
